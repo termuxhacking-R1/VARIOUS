905324c190675252c45f20a8039217df868e6168
Termux Official R1 <65804353+termuxhacking-R1@users.noreply.github.com>
2021-05-24 10:19:02 +0000

    Add files via upload

---
+++ warna.py
@@ -0,0 +1,20 @@
+import random as ran
+import time
+
+stop=False
+def pelangi(word,to=None,tab=None):
+	if tab:
+		ct=["\t" for i in range(tab)]
+	else:
+		ct=''
+	start=time.time()
+	while 1:
+		myl=[f"\033[9{ran.randint(0,9)}m{x}" for x in word]
+		print(end=f"{''.join(ct)}"+f"{''.join(myl)}"+"\r",flush=True)
+		if stop:
+			break
+		elif to:
+			if int(time.time()-start) == int(to):
+				break
+	print()
+
